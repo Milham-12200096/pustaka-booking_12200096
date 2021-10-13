344823b0b24db03e265d949709d379238377ae74
Milham-12200096 <ilhammuhammad0498@gmail.com>
2021-10-13 23:19:40 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,16 @@
+<html lang="">
+   <head>
+       <title>Perpustakaan Login</title>
+   </head>
+   <body>
+
+   <h1>Selamat datang di Sistem Informasi Perpustakaan</h1>
+   <p>
+       Ini adalah layanan perpustakaan digital masa kini
+       <a href="<?=base_url('/daftar-member')?>">klik daftar</a> untuk mendaftar menjadi member
+
+   </p>
+
+
+   </body>
+</html>
